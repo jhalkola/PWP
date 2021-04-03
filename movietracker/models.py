7e6a68c5f495c14590b06348d17f9322a2d27eab
jhalkola <joona.halkola@hotmail.com>
2021-04-03 13:06:33 +0300

    schemas

---
+++ movietracker/models.py
@@ -0,0 +1,96 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-tracker.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+
+    movies = db.relationship("Movie", back_populates="genre")
+    series = db.relationship("Series", back_populates="genre")
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id", ondelete="CASCADE"))
+    title = db.Column(db.String(64), nullable=False)
+    uuid = db.Column(db.String(64), nullable=False, unique=True)
+    actors = db.Column(db.String(64), nullable=True)
+    release_date = db.Column(db.String(64), nullable=True)
+    score = db.Column(db.Float, nullable=True)
+
+    genre = db.relationship("Genre", back_populates="movies")
+
+    @staticmethod
+    def get_schema():
+        schema = {
+            "type": "object",
+            "required": ["title"]
+        }
+        props = schema["properties"] = {}
+        props["title"] = {
+            "description": "Movie name",
+            "type": "string"
+        }
+        props["actors"] =  {
+            "description": "List of actors",
+            "type": "string"
+        }
+        props["release_date"] =  {
+            "description": "Release date of the movie",
+            "type": "string",
+            "pattern": "^[0-3][0-9]-[01][0-9]-[0-9]{4}$"
+        }
+        props["score"] = {
+            "description": "IMDb score of the movie",
+            "type": "number"
+        }
+        return schema
+
+
+class Series(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id", ondelete="CASCADE"))
+    title = db.Column(db.String(64), nullable=False)
+    uuid = db.Column(db.String(64), nullable=False, unique=True)
+    actors = db.Column(db.String(64), nullable=True)
+    release_date = db.Column(db.String(64), nullable=True)
+    score = db.Column(db.Float, nullable=True)
+    seasons = db.Column(db.Integer, default=1, nullable=False)
+
+    genre = db.relationship("Genre", back_populates="series")
+
+    @staticmethod
+    def get_schema():
+        schema = {
+            "type": "object",
+            "required": ["title"]
+        }
+        props = schema["properties"] = {}
+        props["title"] = {
+            "description": "Series name",
+            "type": "string"
+        }
+        props["actors"] =  {
+            "description": "List of actors",
+            "type": "string"
+        }
+        props["release_date"] =  {
+            "description": "Release date of the series",
+            "type": "string",
+            "pattern": "^[0-3][0-9]-[01][0-9]-[0-9]{4}$"
+        }
+        props["score"] = {
+            "description": "IMDb score of the series",
+            "type": "number"
+        }
+        props["seasons"] = {
+            "description": "Number of seasons",
+            "type": "number"
+        }
+        return schema
